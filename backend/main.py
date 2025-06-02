42b0cae74ea9283d98e8a2843e8df8c6628b9756
Param Arora <param2k3@gmail.com>
2025-06-02 15:08:44 +0530

    Db Connection

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import Session
+
+
+from database import get_session
 
 
 app = FastAPI()
 
 
+origins = ["http://localhost", "http://localhost:3000"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
+
+
+
+@app.get("/")
+def root( session: Session = Depends(get_session)):
+    return {"message": "Working"}
 
