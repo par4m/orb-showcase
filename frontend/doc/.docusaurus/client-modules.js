32f21eaaaecdb47b859bb567fc1d6787af51da00
Param Arora <param2k3@gmail.com>
2025-08-21 19:17:29 +0530

    rm doc

--- frontend/doc/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/param/Documents/orb-showcase/frontend/doc/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/param/Documents/orb-showcase/frontend/doc/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/param/Documents/orb-showcase/frontend/doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/param/Documents/orb-showcase/frontend/doc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/param/Documents/orb-showcase/frontend/doc/src/css/custom.css"),
-];
